f93ead14794f66daae9256291394d70a31ec44d4
javiera.izquierdo <javiera.izquierdo@MacBook-Pro-de-javieraizquierdo.local>
2024-06-28 22:42:00 -0400

    mostrar usuarios

--- 01-react-base/src/components/UserPage.tsx
+++ 01-react-base/src/components/UserPage.tsx
@@ -1,8 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from 'axios';
-import { ReqResUserListResponse } from "../interfaces";
+import { ReqResUserListResponse, User } from "../interfaces";
 
-const loadUsers = async () => {
+const loadUsers = async (): Promise<User[]> => {
     try {
         // TODO: el get espera el formato de ReqResUserListResponse y resp los usuarios
         const { data } = await axios.get<ReqResUserListResponse>('https://reqres.in/api/users')
@@ -20,8 +20,14 @@ const loadUsers = async () => {
 
 export const UserPage = () => {
 
+    const [users, setUsers] = useState<User[]>([]);
+
     useEffect(() => {
-        loadUsers().then(users => console.log(users))
+        loadUsers()
+            .then(users => setUsers(users))
+        // TODO es lo mismo que lo de arriba
+        // loadUsers().then ( setUsers )
+
     }, [])
 
     return (
@@ -36,14 +42,35 @@ export const UserPage = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>avatar</td>
-                        <td>nombre</td>
-                        <td>email</td>
-                    </tr>
+                    {
+                        users.map(user => (
+                            <UserRow key={user.id} user={user} />
+                        ))
+                    }
                 </tbody>
             </table>
 
         </>
+    )
+}
+
+
+
+interface Props {
+    user: User;
+}
+
+
+
+export const UserRow = ({ user }: Props) => {
+
+    const { avatar, first_name, last_name, email } = user;
+
+    return (
+        <tr>
+            <td> <img src={avatar} alt="" /> </td>
+            <td> {first_name} {last_name} </td>
+            <td> {email} </td>
+        </tr>
     )
 }
